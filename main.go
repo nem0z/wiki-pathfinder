6bdce7d9c6b4b049a5c96b8e8db234c8b9c84cf6
nem0z <maxime.bernard.dev@gmail.com>
2024-01-28 16:08:07 +0100

    feat (main): implement main function

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/nem0z/wiki-pathfinder/crawler"
+	"github.com/nem0z/wiki-pathfinder/storage"
+)
+
+func Handle(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	db, err := storage.Init("local.db")
+	Handle(err)
+
+	queue := crawler.InitQueue(db)
+	ch := make(chan *crawler.CrawlerResp)
+
+	for i := 0; i < 250; i++ {
+		crawler := crawler.New(queue, ch)
+		go crawler.Work()
+	}
+
+	for {
+		select {
+		case resp := <-ch:
+			db.CreateLinks(resp.ParentId, resp.Childs...)
+		}
+	}
 }
